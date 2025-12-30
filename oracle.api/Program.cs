35c1f77b5eb4b5a94c06da8a7cbd22ce247a7795
root <saturnn29>
2025-12-30 03:45:11 +0000

    modify Program.cs

--- oracle.api/Program.cs
+++ oracle.api/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using oracle.api.Infrastructure.Contexts;
 
 namespace oracle.api
 {
@@ -14,7 +11,29 @@ namespace oracle.api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            // 1. Build the host
+            var host = CreateWebHostBuilder(args).Build();
+
+            // 2. Create a scope to get the database context
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<UserDbContext>();
+                    
+                    // 3. This command triggers the "translator" to create the tables
+                    context.Database.Migrate(); 
+                }
+                catch (Exception ex)
+                {
+                    // If there is a connection error with Oracle, it will show here
+                    Console.WriteLine("An error occurred while migrating the database: " + ex.Message);
+                }
+            }
+
+            // 4. Run the application
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
